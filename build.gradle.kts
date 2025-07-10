e05ae656d41f81c7a0169578c908de5a076930ad
V1nga <43814565+V1nga@users.noreply.github.com>
2025-07-10 13:16:17 +0300

    Configured spring data

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
     id("java")
 }
 
@@ -10,6 +12,9 @@ repositories {
 }
 
 dependencies {
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.telegram:telegrambots-springboot-longpolling-starter:9.0.0")
     implementation("org.telegram:telegrambots-client:9.0.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
